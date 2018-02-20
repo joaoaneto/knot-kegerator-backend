db71eb71be3d0195c0f0b7f5225f6e38c09f5999
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:16:25 -0300

    Add domain rules to setup tap
    Change-Id: I6aca89bc23c2846ee49e0df7670c801c02c82f25

--- src/interactors/SetupTap.js
+++ src/interactors/SetupTap.js
@@ -5,19 +5,28 @@ class SetupTap {
     this.cloudStore = cloudStore;
   }
 
+<<<<<<< eb714961e2ed6880dc10e8f2a55b0eed68be2354
   async execute(setup) {
     const tap = await this.cloudStore.getTap(setup.id);
 
     if (tap.waitingSetup === false) {
+=======
+  async execute(tap) {
+    if (tap.waitingSetup === false) {
+      console.log('erro');
+>>>>>>> Add domain rules to setup tap
       const error = new Error('Tap doesn\'t in setup mode');
       error.code = 403;
       throw error;
     }
+<<<<<<< eb714961e2ed6880dc10e8f2a55b0eed68be2354
 
     tap.setup.client.id = setup.clientId;
     tap.setup.beer.id = setup.beerId;
     tap.setup.keg.id = setup.kegId;
 
+=======
+>>>>>>> Add domain rules to setup tap
     await this.mongoStore.setupTap(tap);
     await this.cloudStore.setupTap(tap);
   }
--- test/interactors/SetupTap.spec.js
+++ test/interactors/SetupTap.spec.js
@@ -34,11 +34,17 @@ function createTest(data) {
   return around(tape)
     .before((t) => {
       const mongoStore = {
+<<<<<<< eb714961e2ed6880dc10e8f2a55b0eed68be2354
         getTap: sinon.stub().resolves(data),
         setupTap: sinon.stub().resolves(),
       };
       const cloudStore = {
         getTap: sinon.stub().resolves(data),
+=======
+        setupTap: sinon.stub().resolves(),
+      };
+      const cloudStore = {
+>>>>>>> Add domain rules to setup tap
         setupTap: sinon.stub().resolves(),
       };
       t.next(mongoStore, cloudStore, data);
