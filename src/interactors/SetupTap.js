4e6ee7adfe57c6d9e0389cf4e8b89cdd5dac1ef8
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:24:16 -0300

    Setup tap on cloud store
    Change-Id: I07ceb1bbce28592e962cf0a47f54e3e9f18c8bed

--- src/interactors/SetupTap.js
+++ src/interactors/SetupTap.js
@@ -5,28 +5,19 @@ class SetupTap {
     this.cloudStore = cloudStore;
   }
 
-<<<<<<< 72d604dd726237e051ce7746cc04733e4b6ffdd2
   async execute(setup) {
     const tap = await this.cloudStore.getTap(setup.id);
 
     if (tap.waitingSetup === false) {
-=======
-  async execute(tap) {
-    if (tap.waitingSetup === false) {
-      console.log('erro');
->>>>>>> Add domain rules to setup tap
       const error = new Error('Tap doesn\'t in setup mode');
       error.code = 403;
       throw error;
     }
-<<<<<<< 72d604dd726237e051ce7746cc04733e4b6ffdd2
 
     tap.setup.client.id = setup.clientId;
     tap.setup.beer.id = setup.beerId;
     tap.setup.keg.id = setup.kegId;
 
-=======
->>>>>>> Add domain rules to setup tap
     await this.mongoStore.setupTap(tap);
     await this.cloudStore.setupTap(tap);
   }
